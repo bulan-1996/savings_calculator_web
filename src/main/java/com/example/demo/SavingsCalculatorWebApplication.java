1111014225b2d2a0d49a1c0d1cc6cb2765482cf7
bulan-1996 <zawashi0730123@gmail.com>
2024-02-08 21:13:20 +0900

    First commit

---
+++ src/main/java/com/example/demo/SavingsCalculatorWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsCalculatorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsCalculatorWebApplication.class, args);
+	}
+
+}
